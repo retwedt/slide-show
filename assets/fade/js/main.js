3b0104fea0a2994684d35d9b13aea3c886569409
retwedt <retwedt@gmail.com>
2016-04-20 18:32:22 -0500

    update fade demo, remove need for requestanimframes

--- assets/fade/js/main.js
+++ assets/fade/js/main.js
@@ -1,83 +1,50 @@
 // main.js
-// from http://www.chrisbuttery.com/articles/fade-in-fade-out-with-javascript/
 
 
 // DOM ELEMENTS
 var imgWrap = document.querySelector(".img-wrap");
 var img = imgWrap.querySelector("img");
+// set image wrap to the exact dimensions of the image
+var h = img.height;
+var w = img.width;
+imgWrap.style.height = h + "px";
+imgWrap.style.width = w + "px";
+// remove the image template
 imgWrap.removeChild(img);
 
 var imgTime = new Date().getTime();
 
 
-// shim layer with setTimeout fallback
-// http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-
-
 // // setup scroller
 updateScroller();
-document.onclick = function() {
-	imgTime = new Date().getTime()
-	updateScroller();
+imgWrap.onclick = function() {
+  // prevent update when clicked too quickly
+  var newTime = new Date().getTime();
+  if ((newTime - imgTime) > 800) {
+  	imgTime = new Date().getTime();
+  	updateScroller();
+  } else {
+    console.log("clicking too quickly!");
+  }
 }
 
 
 // FUNCTIONS
 function updateScroller() {
-	clearScroller();
-	var newImg = img.cloneNode(true);
-  newImg.classList.add('is-hidden');
-
-	imgWrap.appendChild(newImg);
-	setTimeout(function() {fadeIn(newImg)}, 500);
+  clearScroller();
+  var newImg = img.cloneNode(true);
+  newImg.setAttribute("class", "hide");
+  imgWrap.appendChild(newImg);
+  setTimeout(function() {
+    newImg.setAttribute("class", "show");
+  }, 800);
 }
 function clearScroller() {
 	var curImg = imgWrap.querySelector("img");
 	if (curImg) {
-		fadeOut(curImg);
+    curImg.setAttribute("class", "hide");
+    setTimeout(function() {
+      imgWrap.removeChild(curImg);
+    }, 500);
 	}
 }
-function checkTime() {
-	var time = new Date().getTime();
-	if ((time - imgTime) >= 2000) {
-		console.log("check");
-		imgTime = time;
-		updateScroller();
-	}
-}
-function fadeOut(el){
-  el.style.opacity = 1;
-  (function fade() {
-    if ((el.style.opacity -= .1) < 0) {
-      el.style.display = 'none';
-      el.classList.add('is-hidden');
-      el.parentElement.removeChild(el);
-    } else {
-      requestAnimationFrame(fade);
-    }
-  })();
-}
-function fadeIn(el){
-  if (el.classList.contains('is-hidden')){
-    el.classList.remove('is-hidden');
-  }
-  el.style.opacity = 0;
-
-  (function fade() {
-    var val = parseFloat(el.style.opacity);
-    if (!((val += .1) > 1)) {
-      el.style.opacity = val;
-      requestAnimFrame(fade);
-    }
-  })();
-}
-
-
